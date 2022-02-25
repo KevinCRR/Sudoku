7e97c942e03c0e7f2b1d654cfe0d0ac82930ed9b
KevinCRR <47986882+KevinCRR@users.noreply.github.com>
2022-02-24 19:30:04 -0500

    soduko changes

--- scripts/sudoku.js
+++ scripts/sudoku.js
@@ -1,58 +1,97 @@
-//create array based on ints ("1"-"9" with "-1" being a special placeholder for empty cells)
-const numbers = [-1, -1, -1, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+// //create array based on ints ("1"-"9" with "-1" being a special placeholder for empty cells)
+// const numbers = [-1, -1, -1, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-//Add code to the `sudoku.js` file that will dynamically build the game board
-// 1 2 3 4 5 6 7 8 9    2 x 1 = 9, do per row, check content of column. per item pop except for -1
-// 1 2 3 4 5 6 7 8 9
-// 1 2 3 4 5 6 7 8 9
-// 1 2 3 4 5 6 7 8 9
-// 1 2 3 4 5 6 7 8 9
-// 1 2 3 4 5 6 7 8 9
+// //Add code to the `sudoku.js` file that will dynamically build the game board
+// // 1 2 3 4 5 6 7 8 9    2 x 1 = 9, do per row, check content of column. per item pop except for -1
+// // 1 2 3 4 5 6 7 8 9
+// // 1 2 3 4 5 6 7 8 9
+// // 1 2 3 4 5 6 7 8 9
+// // 1 2 3 4 5 6 7 8 9
+// // 1 2 3 4 5 6 7 8 9
 
-// create rand int, check all rows in the same column if number exists,
-// if it exist create new rand int, go back
-// if it doesn't assing int, pop num out of permanent list and move on.
-function assign_table() {
-  let matrix = [[], [], [], [], [], [], [], [], []];
-  let cols = 9;
-  let flag = false;
-  let otherflag = false;
+// // create rand int, check all rows in the same column if number exists,
+// // if it exist create new rand int, go back
+// // if it doesn't assing int, pop num out of permanent list and move on.
+// function assign_table() {
+//   let matrix = [[], [], [], [], [], [], [], [], []];
+//   let cols = 9;
+//   let flag = false;
+//   let otherflag = false;
+
+//   for (var i = 0; i < cols; i++) {
+//     for (var x = 0; x < cols; x++) {
+//       var number = randnums();
+//       var a = i - 1;
+//       if (matrix[i].indexOf(number) != -1) {
+//         number = randnums();
+//         x = x - 1;
+//       }
+
+//       if (i > 0) {
+//         for (var z = 0; z < a; z++) {
+//           if (matrix[z][x] == number && matrix[z][x] != -1 && z > 0) {
+//             console.log("before: ", z);
+//             console.log("Number: ", number);
+//             console.log("matrixr: ", matrix[z][x]);
+//             console.log("row: ", i);
+//             console.log("col: ", x);
+//             number = randnums();
+//             z = z - 1;
+//             console.log("after: ", z);
+//           }
+//         }
+//       }
+
+//       matrix[i][x] = number;
+//     }
+//   }
+//   //   const index = matrix.indexOf(1);
+//   //   console.log(index);
+//   return matrix;
+// }
+
+// function randnums() {
+//   let randomItem = numbers[Math.floor(Math.random() * numbers.length)];
+//   return randomItem;
+// }
+
+// console.log(assign_table());
+function clickHandler() {
+  // Here, `this` refers to the element the event was hooked on
+  console.log("clicked");
+}
+
+function createTable() {
+  var table = document.getElementById("board");
+  let tableValues = [
+    [-1, 1, -1, -1, -1, -1, -1, 9, -1],
+    [-1, -1, 4, -1, -1, -1, 2, -1, -1],
+    [-1, -1, 8, -1, -1, 5, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, 3, -1],
+    [2, -1, -1, -1, 4, -1, 1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, 1, 8, -1, -1, 6, -1, -1],
+    [-1, 3, -1, -1, -1, -1, -1, 8, -1],
+    [-1, -1, 6, -1, -1, -1, -1, -1, -1],
+  ];
+  const cols = 9;
+  var row = [];
+  var cell = [];
 
   for (var i = 0; i < cols; i++) {
+    row[i] = table.insertRow(i);
     for (var x = 0; x < cols; x++) {
-      var number = randnums();
-      var a = i - 1;
-      if (matrix[i].indexOf(number) != -1) {
-        number = randnums();
-        x = x - 1;
+      let name = "cell" + (i + 1) + (x + 1);
+      let val = String(i + 1) + String(x + 1);
+      cell[x] = row[i].insertCell(x);
+      if (tableValues[i][x] == -1) {
+        cell[x].setAttribute(name, val);
+        cell[x].addEventListener("click", clickHandler);
+        cell[x].innerHTML = " ";
+      } else {
+        cell[x].setAttribute(name, val);
+        cell[x].innerHTML = tableValues[i][x];
       }
-
-      if (i > 0) {
-        for (var z = 0; z < a; z++) {
-          if (matrix[z][x] == number && matrix[z][x] != -1 && z > 0) {
-            console.log("before: ", z);
-            console.log("Number: ", number);
-            console.log("matrixr: ", matrix[z][x]);
-            console.log("row: ", i);
-            console.log("col: ", x);
-            number = randnums();
-            z = z - 1;
-            console.log("after: ", z);
-          }
-        }
-      }
-
-      matrix[i][x] = number;
     }
   }
-  //   const index = matrix.indexOf(1);
-  //   console.log(index);
-  return matrix;
 }
-
-function randnums() {
-  let randomItem = numbers[Math.floor(Math.random() * numbers.length)];
-  return randomItem;
-}
-
-console.log(assign_table());
