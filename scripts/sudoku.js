2943160ca385f1048fef8d43adc8586d7e1204ae
KevinCRR <47986882+KevinCRR@users.noreply.github.com>
2022-03-01 09:53:30 -0500

    Almost done

--- scripts/sudoku.js
+++ scripts/sudoku.js
@@ -1,4 +1,5 @@
-// //create array based on ints ("1"-"9" with "-1" being a special placeholder for empty cells)
+// ATTEMPT AT MAKING SELF GENERATING SUDOKU BOARD
+//create array based on ints ("1"-"9" with "-1" being a special placeholder for empty cells)
 // const numbers = [-1, -1, -1, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 // //Add code to the `sudoku.js` file that will dynamically build the game board
@@ -56,15 +57,90 @@
 // }
 
 // console.log(assign_table());
-function clickHandler() {
-  // Here, `this` refers to the element the event was hooked on
-  window.onclick = e => {
-    console.log(e.target.innerText);
-    } 
+
+var clickedValue;
+var previousElements = [];
+var previousValues = [];
+
+function ValueSelect(e) {
+  clickedValue = e.target.innerText;
 }
 
+function valueAssign(e) {
+  if (clickedValue === undefined) {
+  } else if (!(e.target.innerText == "")) {
+  } else if (e.target.getElementsByTagName("img").length > 0) {
+  } else {
+    previousElements.push(e);
+    previousValues.push(e.target.innerText);
+    e.target.innerText = clickedValue;
+    sameRow(e.target);
+    clickedValue = undefined;
+  }
+}
+
+function previousValue() {
+  if (previousElements.length > 0) {
+    previousElements.at(-1).target.innerText = previousValues.at(-1);
+    previousElements.pop();
+    previousValues.pop();
+  }
+}
+
+//change values from var to let
+//could optimize by having loop check for inner block items, if not found just loop through same row and col of other blocks
+//maybe seperate row and colm checker from blocker checker?
+//maybe have a better naming convention?
+function sameRow(currentCell) {
+  var currentCellNumber = currentCell.getAttribute("cell");
+  let gametable = document.getElementById("board");
+  var currentCellRow = parseInt(currentCellNumber.charAt(0)) - 1;
+  var currentCelCol = parseInt(currentCellNumber.charAt(1)) - 1;
+
+  var Gamerow = gametable.rows[currentCellRow];
+  var GameCol;
+  var dynamicRow;
+  var staticCol;
+  for (var i = 0; i < 9; i++) {
+    dynamicRow = gametable.rows[i];
+    staticCol = dynamicRow.cells[currentCelCol];
+    GameCol = Gamerow.cells[i];
+    if (currentCelCol == i) {
+    } else if (GameCol.innerText == clickedValue) {
+      console.log("same number found in row");
+    }
+
+    if (currentCellRow == i) {
+    } else if (staticCol.innerText == clickedValue) {
+      console.log("same number found in col");
+    }
+  }
+
+  let firstRow = Math.floor(currentCellRow / 3) * 3;
+  let firstCol = Math.floor(currentCelCol / 3) * 3;
+  let lastRow = firstRow + 3;
+  let lastCol = firstCol + 3;
+  let blockRow;
+  let blockCol;
+
+  for (var i = firstRow; i < lastRow; i++) {
+    blockRow = gametable.rows[i];
+    for (var x = firstCol; x < lastCol; x++) {
+      blockCol = blockRow.cells[x];
+      if (currentCellRow == i && currentCelCol == x) {
+      } else if (blockCol.innerText == clickedValue) {
+        console.log("same number found in block");
+      }
+    }
+  }
+}
+
+function startGame() {
+  createNumberPalette();
+  createTable();
+}
 function createTable() {
-  var table = document.getElementById("board");
+  let gametable = document.getElementById("board");
   let tableValues = [
     [-1, 1, -1, -1, -1, -1, -1, 9, -1],
     [-1, -1, 4, -1, -1, -1, 2, -1, -1],
@@ -77,23 +153,45 @@ function createTable() {
     [-1, -1, 6, -1, -1, -1, -1, -1, -1],
   ];
   const cols = 9;
-  var row = [];
-  var cell = [];
+  let row = [];
+  let cell = [];
 
   for (var i = 0; i < cols; i++) {
-    row[i] = table.insertRow(i);
+    row[i] = gametable.insertRow(i);
     for (var x = 0; x < cols; x++) {
-      let name = "cell" + (i + 1) + (x + 1);
       let val = String(i + 1) + String(x + 1);
       cell[x] = row[i].insertCell(x);
       if (tableValues[i][x] == -1) {
-        cell[x].setAttribute(name, val);
-        cell[x].addEventListener("click", clickHandler);
+        cell[x].setAttribute("cell", val);
+        cell[x].addEventListener("click", valueAssign);
         cell[x].innerHTML = " ";
       } else {
-        cell[x].setAttribute(name, val);
+        cell[x].setAttribute("cell", val);
+        cell[x].addEventListener("click", valueAssign);
         cell[x].innerHTML = tableValues[i][x];
       }
+    }
+  }
+}
+
+function createNumberPalette() {
+  var palette = document.getElementById("palette");
+  let row = palette.insertRow(0);
+  var paletteValues = [1, 2, 3, 4, 5, 6, 7, 8, 9, "Back"];
+  const paletteRows = 10;
+  let col = [];
+
+  for (var i = 0; i < paletteRows; i++) {
+    if (i === 9) {
+      col[i] = row.insertCell(i);
+      col[i].addEventListener("click", previousValue);
+      col[i].appendChild(document.createElement("img")).src =
+        "./images/undo.png";
+    } else {
+      col[i] = row.insertCell(i);
+      col[i].addEventListener("click", ValueSelect);
+      col[i].setAttribute("tabindex", i);
+      col[i].innerHTML = paletteValues[i];
     }
   }
 }
